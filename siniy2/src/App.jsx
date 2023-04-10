5d5a2ce78d472cbb66d48578bc565900c8473a87
andfff <124504338+andfff@users.noreply.github.com>
2023-04-10 21:31:38 +0300

    Add files via upload

---
+++ siniy2/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './header/header';
+import Content from './content/content';
+function App() {
+  return (
+    <div className="main-container">
+      <Header />
+      <div className="float_name">SIniy Vlad</div>
+      <Content />
+    </div>
+  )
+}
+
+export default App;
